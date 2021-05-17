bbe4cc52d8d8c13f3a0725d8b8483d90c794ef46
donellgill <donell.gill@outlook.com>
2021-05-16 21:08:49 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe() {
+    
+    function Blurred(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    function Focused(){
+        console.log("Good!")
+    }
+
+return (
+    <div>
+        <button onBlur= {Blurred} onFocus ={Focused}>Eyes on me</button>
+    </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from "react"
+
+function Keypad() {
+    
+    function handleChange(){
+        console.log('Entering password...')
+    }
+
+return (
+    <div>
+        <input type = "password" placeholder= "password" onChange ={handleChange}></input>
+    </div>
+    )
+}
+
+export default Keypad;
